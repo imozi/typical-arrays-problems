9a2785a98d9948d1f91766aa8b73017af3b33eff
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2020-09-29 10:44:32 +1200

    feat: add function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length === 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    return array.reduce((a, e) => {
+        return a < e ? a : (a = e);
+    });
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length === 0) return 0;
+    return array.reduce((a, e) => {
+        return a > e ? a : (a = e);
+    });
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length === 0) return 0;
+
+    const sum = array.reduce((a, e) => {
+        return a + e;
+    });
+
+    return sum / array.length;
+};
